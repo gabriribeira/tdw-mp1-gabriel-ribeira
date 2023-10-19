b8901ef2421cc9e740bfe35da4b51f331957f4af
gabriribeira <gmrribeira@gmail.com>
2023-10-19 13:56:27 +0100

    workflow on merge test using github actions

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/" legacyBehavior>
-        <a className="hover:underline">Blog do Gabriel</a>
+        <a className="hover:underline">Blog do Gabriel Ribeira</a>
       </Link>
       .
     </h2>
